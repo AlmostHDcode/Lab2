c5c690f24044a2e72100d3fd9cadccc80f1ad393
Matt Hunt <hmatt33@gmail.com>
2019-02-28 09:44:21 -0500

    Hello uses Greeter

---
+++ lib/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+	public static void main(String argv[]) {
+		//Default is "World"
+		String name = "World";
+		if(argv.length != 0) {
+			name = argv[0];
+		}
+		Greeter greeter = new Greeter(name);
+		System.out.println(greeter.greet());
+	}
+}
